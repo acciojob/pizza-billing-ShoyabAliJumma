a68d0871d611b61d6eca3db2222a6598a67e38b9
ShoyabAliJumma <shoyabalipathan@gmail.com>
2023-01-19 02:17:43 +0530

    solution1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,13 +1,26 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Pizza {
 
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean CheeseEx;
+    private boolean Toppings;
+    private boolean TakeAway;
+    private int total;
+    //Veg pizza base price = 300
+    //Non-veg pizza base price = 400
+    //Extra Cheese Price = 80
+    //Extra Toppings For Veg Pizza = 70
+    //Extra Toppings For Non-veg Pizza = 120
+    //Paper bag Price = 20
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        price = this.isVeg?300:400;
         // your code goes here
     }
 
@@ -17,18 +30,40 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (!CheeseEx){
+            CheeseEx = true;
+            price +=80;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!Toppings){
+            Toppings=true;
+            price += isVeg?70:120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+
+        if(!TakeAway){
+            TakeAway = true;
+            price += 20;
+        }
+
     }
 
     public String getBill(){
         // your code goes here
+
+        //Base Price Of The Pizza: 300
+        //Extra Cheese Added: 80
+        //Extra Toppings Added: 70
+        //Paperbag Added: 20
+        //Total Price: 470
+        bill = "Base Price Of The Pizza: "+(isVeg?300:400)+"\n"+(CheeseEx?("Extra Cheese Added: "+80+"\n"):"")+(Toppings?("Extra Toppings Added: "+(isVeg?70:120)+"\n"):"")+
+                (TakeAway?("Paperbag Added: "+20+"\n"):"")+"Total Price: "+price+"\n";
         return this.bill;
     }
 }
